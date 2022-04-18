9fa728cab15052c1e80767c994594f95c7eaa8f9
StanOswald <stan_oswald@qq.com>
2022-04-18 19:58:59 +0800

    Add DataPlane

--- src/main/java/checker/DataPlane.java
+++ src/main/java/checker/DataPlane.java
@@ -1,12 +1,30 @@
 package checker;
 
+import algorithm.Detection;
 import process.BGPMessage;
 import process.DetectionResult;
 
-public class DataPlane implements BasicChecker {
+import java.util.List;
+
+public class DataPlane extends Detection implements BasicChecker {
+
+    int threshold;
+
+    public DataPlane(int threshold) {
+        this.threshold = threshold;
+    }
 
     @Override
     public DetectionResult hijackCheck(BGPMessage message) {
-        return null;
+        int u = 0;
+        List<Integer> path = message.getPath();
+        for (Integer ASn : path) {
+            if (!connectivityCheck(ASn)) {
+                u++;
+                if (u > threshold)
+                    return new DetectionResult().setResult(true).setType(6);
+            }
+        }
+        return new DetectionResult().setResult(false);
     }
 }
