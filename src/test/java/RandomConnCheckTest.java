76c3d1a36dee3a8a5725538ffca0dd38c972252f
StanOswald <stan_oswald@qq.com>
2022-05-18 16:46:30 +0800

    Optimize connectivity checker #2 Add ipv6 support to junit; start using ipaddress lib from seancfoley

--- src/test/java/RandomConnCheckTest.java
+++ src/test/java/RandomConnCheckTest.java
@@ -1,10 +1,17 @@
 import algorithm.Detection;
+import inet.ipaddr.AddressStringException;
+import inet.ipaddr.IPAddress;
+import inet.ipaddr.IPAddressString;
 import org.apache.commons.net.util.SubnetUtils;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -13,7 +20,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class RandomConnCheckTest extends Detection {
     @Test
     public void mainTest() throws InterruptedException {
-        List<String> announcedPrefixes = getAnnouncedPrefixes(23911);
+        List<String> announcedPrefixes = getAnnouncedPrefixes(133774);
         if (announcedPrefixes.size() == 0) {
             System.out.println("not visible");
             return;
@@ -24,7 +31,8 @@ public class RandomConnCheckTest extends Detection {
         System.out.println("Start checking");
         for (String ip : announcedPrefixes) {
             if (ip.contains(":")) {
-                System.out.println("IPv6 no supported");
+                // System.out.println("IPv6 not supported");
+                System.out.println("IPv6 now supported!");
                 continue;
             }
             System.out.println("Trying subnet: " + ip);
@@ -33,6 +41,7 @@ public class RandomConnCheckTest extends Detection {
             SubnetUtils.SubnetInfo info = subnetUtils.getInfo();
             if (info.getAddressCountLong() > 60000)
                 continue;
+
             String[] allAddresses = info.getAllAddresses();
 
             CountDownLatch latch = new CountDownLatch(allAddresses.length);
@@ -56,8 +65,51 @@ public class RandomConnCheckTest extends Detection {
             latch.await();
         }
 
-
         System.out.println("Finished");
     }
 
+
+    @Test
+    public void IPv6Test() throws AddressStringException, InterruptedException {
+        List<String> announcedPrefixes = getAnnouncedPrefixes(133774);
+        if (announcedPrefixes.size() == 0) {
+            System.out.println("not visible");
+            return;
+        }
+
+        AtomicBoolean succeed = new AtomicBoolean(false);
+        System.out.println("Start checking");
+        // announcedPrefixes = List.of("240e:108:1180::/48");
+        for (String ip : announcedPrefixes) {
+
+            IPAddressString ipStr = new IPAddressString(ip);
+            IPAddress ipAddr = ipStr.toAddress();
+
+
+            CountDownLatch latch = new CountDownLatch(1);
+            for (IPAddress address : ipAddr.getIterable()) {
+                InetAddress inetAddr = address.toInetAddress();
+                new Thread(() -> {
+                    try {
+                        if (inetAddr.isReachable(NetworkInterface.getByName("eth8"),256,500)) {
+                            System.out.println("Trying: " + address + " success");
+                            succeed.set(true);
+                            latch.countDown();
+                        } else System.out.println("Trying: " + address);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+
+                if (succeed.get())
+                    break;
+            }
+            latch.await();
+        }
+        System.out.println("Finished");
+    }
+
+    @Test
+    public void netIfTest() throws SocketException {
+    }
 }
